93419ee8e5de7d7e468cc8a6654b0f080c509c51
Alex Ostrovski <ostrovski.alex@gmail.com>
2022-04-12 10:50:07 +0300

    Update TypeScript declarations

--- packages/julia-set-node/index.d.ts
+++ packages/julia-set-node/index.d.ts
@@ -1,22 +1,84 @@
 import { Options } from 'julia-set';
 import { Browser } from 'puppeteer';
 
+/**
+ * Options for taking screenshots of Julia sets.
+ */
 export declare interface ScreenshotOptions {
+  /** Width of the screenshot in pixels. */
   width: number,
+  /** Height of the screenshot in pixels. */
   height: number,
+
+  /**
+   * Browser instance to use to generate the screenshot.
+   * If not specified, the new browser instance will be created automatically.
+   */
   browser?: Browser,
+
+  /**
+   * Quality of the screenshot image, in the range between 1 and 100 inclusive.
+   * Only applicable to JPEG images.
+   */
   quality?: number,
+  /**
+   * Filesystem path to save the snapshot to. If specified,
+   * the `encoding` setting will be ignored. If not specified, the generated snapshot
+   * will be asynchronously returned instead of being saved to the file system.
+   */
   path?: string,
+  /**
+   * Type of the generated image. If not specified, defaults to `'jpeg'`.
+   */
   type?: 'png' | 'jpeg',
+  /**
+   * Encoding of the returned image `Promise`. Binary encoding means returning the image data
+   * as a `Uint8Array`, and `'base64'` means encoding it into a base64 string.
+   * If not specified, defaults to `'binary'`.
+   */
   encoding?: 'binary' | 'base64',
 }
 
-declare type SaveScreenshotOptions = { screenshot: ScreenshotOptions & { path: string }};
-declare type BinaryScreenshotOptions = { screenshot: ScreenshotOptions & { path?: never, encoding?: 'binary' }};
-declare type Base64ScreenshotOptions = { screenshot: ScreenshotOptions & { path?: never, encoding: 'base64' }};
-
+/**
+ * Options for saving a Julia set image to the file system.
+ */
+declare type SaveScreenshotOptions = {
+  /** Screenshot options. */
+  screenshot: ScreenshotOptions & { path: string },
+};
+/**
+ * Renders a Julia set and saves the resulting image to the file system.
+ *
+ * @param options rendering options
+ */
 declare function render(options: Options & SaveScreenshotOptions): Promise<void>;
-declare function render(options: Options & Base64ScreenshotOptions): Promise<string>;
+
+/**
+ * Options for generating a Julia set image as `Uint8Array` image data.
+ */
+declare type BinaryScreenshotOptions = {
+  /** Screenshot options. */
+  screenshot: ScreenshotOptions & { path?: never, encoding?: 'binary' },
+};
+/**
+ * Renders a Julia set and returns the resulting image as `Uint8Array` image data.
+ *
+ * @param options rendering options
+ */
 declare function render(options: Options & BinaryScreenshotOptions): Promise<Uint8Array>;
+
+/**
+ * Options for generating a Julia set image as base64-encoded image data.
+ */
+declare type Base64ScreenshotOptions = {
+  /** Screenshot options. */
+  screenshot: ScreenshotOptions & { path?: never, encoding: 'base64' },
+};
+/**
+ * Renders a Julia set and returns the resulting image as base64-encoded image data.
+ *
+ * @param options rendering options
+ */
+declare function render(options: Options & Base64ScreenshotOptions): Promise<string>;
 
 export default render;
--- packages/julia-set/index.d.ts
+++ packages/julia-set/index.d.ts
@@ -1,28 +1,106 @@
-declare type NamedPalette = 'grayscale';
+declare type NamedPalette = 'grayscale' | 'tree' | 'gold' | 'snow' | 'red' | 'green';
+
+/**
+ * Palette specification to use when rendering a Julia set. Can be specified
+ * as either a `NamedPalette`, or as a sequence of stop colors in RGB(A) format
+ * (each channel ranging from 0 to 255 inclusive), kind of like when specifying
+ * a CSS gradient. In the latter case, the palette will be interpolated based
+ * on the provided colors.
+ */
 declare type Palette = NamedPalette | [number, number, number, number?][];
 
+/**
+ * Julia set rendering options.
+ */
 export interface Options {
+  /**
+   * Code of the function to use, with `z` being a free variable.
+   *
+   * Numbers are denoted using infix notation; e.g., `0.2i0.5` denotes a complex number
+   * with real part `0.2` and imaginary part `0.5i`. Besides arithmetic operations
+   * `+`, `-`, `*`, `/` and `^`, some unary complex-valued functions are supported;
+   * see the package readme for details.
+   */
   code: string,
+
+  /**
+   * Center of the rendered rectangular viewport specified as a real part
+   * and imaginary part tuple. If not specified, defaults to `[0, 0]`.
+   */
   center?: [number, number],
+  /**
+   * Height of the rendered rectangular viewport. If not specified, defaults to `1`.
+   */
   height?: number,
+  /**
+   * `Palette` to use to visualize the Julia set.
+   *
+   * The palette is used to map the number of iterations (specified by the `iterations` option)
+   * necessary for a particular pixel in the viewport to escape to "infinity"
+   * (determined by the `runawayDistance` option) when sequentially applying
+   * the complex-valued function specified by the `code` option.
+   *
+   * If not specified, defaults to `'grayscale'`.
+   */
   palette?: Palette,
+
+  /**
+   * Maximum number of iterations when calculating runaway behavior of points
+   * in the viewport. Greater values reveal more details, but the image
+   * may become desaturated.
+   *
+   * If not specified, defaults to `60`.
+   */
   iterations?: number,
+  /**
+   * Threshold magnitude of complex values to consider "infinite".
+   * In some cases, increasing it may reveal fractal details.
+   *
+   * If not specified, defaults to `4`.
+   */
   runawayDistance?: number,
+
+  /**
+   * Whether to perform smoothing for outlier points of the rendered image,
+   * which are usually caused by loss of precision during calculations.
+   *
+   * If not specified, defaults to `false`.
+   */
   antialias?: boolean,
 }
 
-export interface UpdateOptions {
-  code?: string,
-  center?: [number, number],
-  height?: number,
-  palette?: Palette,
-  iterations?: number,
-  runawayDistance?: number,
-}
+/**
+ * Partial `Options` that can be used to update a rendered Julia set.
+ */
+declare type UpdateOptions = Partial<Omit<Options, 'antialias'>>;
 
+/**
+ * Rendered Julia set.
+ */
 declare class JuliaSet {
+  /**
+   * One-off utility method to render a Julia set with the specified `options`
+   * on the `element`.
+   *
+   * @param element canvas to use to render the set
+   * @param options Julia set options
+   */
   static render(element: HTMLCanvasElement, options: Options): JuliaSet;
+
+  /**
+   * Constructs and renders a new Julia set.
+   *
+   * @param element canvas to use to render the set
+   * @param options Julia set options
+   */
   constructor(element: HTMLCanvasElement, options: Options);
+
+  /**
+   * Updates the parameters of the Julia set and re-renders it
+   * on the same canvas element.
+   *
+   * @param options updated Julia set options
+   */
   update(options?: UpdateOptions): void;
 }
 
