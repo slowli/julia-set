f2b1064baeb0b4a4d581b36548095766e6c214ca
Alex Ostrovski <ostrovski.alex@gmail.com>
2022-09-27 17:38:56 +0300

    Update Jest configs

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,18 @@ module.exports = {
     '<rootDir>/packages/**/*.spec.js',
     '**/julia-set-node/test/*.spec.ts',
   ],
+  testPathIgnorePatterns: [
+    // Ignore `tsc`-produced JS file (requires `jest-puppeteer` env;
+    // tested among E2E tests)
+    'julia-set/test/types.spec.js',
+  ],
   coverageDirectory: '<rootDir>/coverage/',
   coveragePathIgnorePatterns: [
     '/julia-set-node/lib/',
     'e2e-tests',
   ],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
   transform: {
     '\\.js$': 'babel-jest',
-    '\\.ts$': 'ts-jest',
+    '\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 };
